101d6c682c7568d45ed09e372d2a056cb38ff68d
Benedikt Werner <1benediktwerner@gmail.com>
2017-10-20 16:16:01 +0200

    Using new master and minion daemon for autosign tests

--- tests/integration/daemons/test_masterapi.py
+++ tests/integration/daemons/test_masterapi.py
@@ -5,9 +5,11 @@ from __future__ import absolute_import
 import os
 import shutil
 import stat
+import time
 
 # Import Salt Testing libs
 from tests.support.case import ShellCase
+from tests.support.paths import TMP
 from tests.integration.utils import testprogram
 
 # Import 3rd-party libs
@@ -27,49 +29,68 @@ class AutosignGrainsTest(ShellCase, testprogram.TestProgramCase):
 
     def setUp(self):
         super(ShellCase, self).setUp()
-
-        self.master = testprogram.TestDaemonSaltMaster(
-            name='autosign_grains_accept_master',
-            parent_dir=self._test_dir,
-        )
-        # Call setup here to ensure config and script exist
-        self.master.setup()
-        self.master.run('-d')
-        self.master.shutdown(wait_for_orphans=3)
-
-        self.run_key('-d minion -y')
-        self.run_call('test.ping -l quiet')  # get minon to try to authenticate itself again
-
-        if 'minion' in self.run_key('-l acc'):
-            self.skipTest('Could not deauthorize minion')
-        if 'minion' not in self.run_key('-l un'):
-            self.skipTest('minion did not try to reauthenticate itself')
-
-        self.autosign_grains_dir = os.path.join(self.master_opts['autosign_grains_dir'])
+        self.autosign_grains_dir = os.path.join(TMP, 'autosign_grains_dir')
         if not os.path.isdir(self.autosign_grains_dir):
             os.makedirs(self.autosign_grains_dir)
 
-    def tearDown(self):
-        self.run_call('test.ping -l quiet')  # get minon to authenticate itself again
+        self.master = testprogram.TestDaemonSaltMaster(
+            name='autosign_grains_master',
+            configs={'master': {'map': {'autosign_grains_dir': self.autosign_grains_dir}}},
+            parent_dir=self._test_dir,
+        )
+        self.master.setup()
+        _, status = self.master.run(args=['-d'], with_retcode=True)
+        if status != 0:
+            self.skipTest("Failed to start master daemon")
 
+        self.minion = testprogram.TestDaemonSaltMinion(
+            name='autosign_grains_minion',
+            configs={'minion': {'map': {
+                'autosign_grains': ['test_grain'],
+                'grains': {'test_grain': 'test_value'}
+            }}},
+            parent_dir=self._test_dir,
+        )
+        self.minion.setup()
+        _, status = self.minion.run(args=['-d'], with_retcode=True)
+        if status != 0:
+            self.skipTest("Failed to start minion daemon")
+
+        time.sleep(10)
+        self.master_conf_dir = self.master.abs_path(self.master.config_dir)
+        print(self.run_script('salt-key', '-c {} -L'.format(self.master_conf_dir)))
+
+    def tearDown(self):
+        self.master.shutdown()
+        self.minion.shutdown()
         if os.path.isdir(self.autosign_grains_dir):
             shutil.rmtree(self.autosign_grains_dir)
+        super(ShellCase, self).tearDown()
 
     def test_autosign_grains_accept(self):
         grain_file_path = os.path.join(self.autosign_grains_dir, 'test_grain')
         with salt.utils.files.fopen(grain_file_path, 'w') as f:
-            f.write('#invalid_value\ncheese')
+            f.write('#invalid_value\ntest_value')
         os.chmod(grain_file_path, autosign_file_permissions)
 
         self.run_call('test.ping -l quiet')  # get minon to try to authenticate itself again
-        self.assertIn('minion', self.run_key('-l acc'))
+        accepted = self.run_script('salt-key', '-c {} -l acc'.format(self.master_conf_dir))
+        print('accepted:', accepted)
+        self.assertIn('minion', accepted)
+        pending = self.run_script('salt-key', '-c {} -l un'.format(self.master_conf_dir))
+        print('pending:', pending)
+        self.assertNotIn('minion', pending)
 
     def test_autosign_grains_fail(self):
         grain_file_path = os.path.join(self.autosign_grains_dir, 'test_grain')
         with salt.utils.files.fopen(grain_file_path, 'w') as f:
-            f.write('#cheese\ninvalid_value')
+            f.write('#test_value\ninvalid_value')
         os.chmod(grain_file_path, autosign_file_permissions)
 
         self.run_call('test.ping -l quiet')  # get minon to try to authenticate itself again
-        self.assertNotIn('minion', self.run_key('-l acc'))
-        self.assertIn('minion', self.run_key('-l un'))
+        accepted = self.run_script('salt-key', '-c {} -l acc'.format(self.master_conf_dir))
+        print('accepted:', accepted)
+        self.assertNotIn('minion', accepted)
+        pending = self.run_script('salt-key', '-c {} -l un'.format(self.master_conf_dir))
+        print('pending:', pending)
+        self.assertIn('minion', pending)
